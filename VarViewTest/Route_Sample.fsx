25c3c3d2f7d11dff68aea2d5df6b67681c69099f
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-03-23 16:31:56 +0000

    Added route sample

--- VarViewTest/Route_Sample.fsx
+++ VarViewTest/Route_Sample.fsx
@@ -11,8 +11,41 @@ open WebSharper.UI.Next.Client
 [<JavaScript>]
 module Client =
     
+    type DesignerEndpoint =
+        | Forms
+        | Form     of formId: string
+        | Sections of formId: string
+        | Section  of formId: string * sectionId: string
+    
     let main() =
-        Doc.Empty
+        let map =
+            RouteMap.Create 
+                (function
+                 | Forms                       -> [ "forms" ]
+                 | Form formId                 -> [ "forms"; formId ]
+                 | Sections formId             -> [ "forms"; formId; "sections" ]
+                 | Section (formId, sectionId) -> [ "forms"; formId; "sections"; sectionId ]) 
+                (function
+                 | [ "forms" ]                                -> Forms
+                 | [ "forms"; formId ]                        -> Form formId
+                 | [ "forms"; formId; "sections" ]            -> Sections formId
+                 | [ "forms"; formId; "sections"; sectionId ] -> Section (formId, sectionId)
+                 | _                                          -> Forms)
+        
+        let route =
+            RouteMap.Install map
+
+        [ Doc.Button "Go Hello" [] (fun () -> Var.Set route (Form "hello")) :> Doc
+          Doc.Button "Go World" [] (fun () -> Var.Set route (Section ("hello", "world"))) :> Doc
+          
+          route.View
+          |> Doc.BindView (function 
+                           | Forms                       -> Doc.Empty
+                           | Form formId                 -> Doc.Empty
+                           | Sections formId             -> Doc.Empty
+                           | Section (formId, sectionId) -> Doc.Empty) ]
+        |> Doc.Concat
+
 
 module Server =
 

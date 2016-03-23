65188040c378f2ab4c3dfdee4d9cab28e396ebfe
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-03-23 15:43:57 +0000

    Creating sample for Route UI.Next

---
+++ VarViewTest/Route_Sample.fsx
@@ -0,0 +1,30 @@
+﻿#I "../packages/"
+#load "WebSharper.Warp/tools/reference-nover.fsx"
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.Sitelets
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Html
+open WebSharper.UI.Next.Client
+
+[<JavaScript>]
+module Client =
+    
+    let main() =
+        Doc.Empty
+
+module Server =
+
+    type Page = { Body: Doc list }
+
+    let template =
+        Content.Template<Page>(__SOURCE_DIRECTORY__ + "/index.html")
+            .With("body", fun x -> x.Body)
+    
+    let site =
+        Application.SinglePage (fun _ ->
+            Content.WithTemplate template
+                { Body = [ client <@ Client.main() @> ] })
+
+do Warp.RunAndWaitForInput Server.site |> ignore
